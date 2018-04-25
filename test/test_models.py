85bfede6584a4a20a430b9cd19a16a2218abdb38
Neil Raymond <ngraymon@uwaterloo.ca>
2018-04-25 18:04:38 -0400

    Ignore the output files generated from tests

--- test/test_models.py
+++ test/test_models.py
@@ -18,13 +18,12 @@ class TestDataSet0Rho0(unittest.TestCase):
         self.id_data = 0
         self.id_rho = 0
 
-        np.random.seed(232942)  # pick our seed
-        self.samples = int(1e2)
-        self.block_size = int(1e2)
+        np.random.seed(242351)  # pick our seed
+        self.samples = int(1e1)
+        self.block_size = int(1e1)
         self.test_path = '/home/ngraymon/test/Pibronic/test/test_models/'
 
     def test_load(self):
-
         # load the relevant data
         data = pm.BoxData()
         data.id_data = self.id_data
@@ -39,7 +38,7 @@ class TestDataSet0Rho0(unittest.TestCase):
         data.modes = 2
 
         data.samples = self.samples
-        data.beads = 1000
+        data.beads = 10
         data.temperature = 300.0
         data.blocks = self.samples // self.block_size
         data.block_size = self.block_size
@@ -55,39 +54,47 @@ class TestDataSet0Rho0(unittest.TestCase):
 
         return
 
-# def data_set_0_test(id_data, id_rho=0):
-#     """Just do simple expval(Z) calculation"""
-#     np.random.seed(232942)  # pick our seed
-#     samples = int(1e2)
-#     Bsize = int(1e2)
 
-#     # load the relevant data
-#     data = BoxData()
-#     data.id_data = id_data
-#     data.id_rho = id_rho
+class TestDataSet1Rho0(unittest.TestCase):
+    def setUp(self):
+        self.id_data = 1
+        self.id_rho = 0
 
-#     files = file_structure.FileStructure('/work/ngraymon/pimc/', id_data, id_rho)
-#     data.path_vib_model = files.path_vib_model
-#     data.path_rho_model = files.path_rho_model
+        np.random.seed(242351)  # pick our seed
+        self.samples = int(1e1)
+        self.block_size = int(1e1)
+        self.test_path = '/home/ngraymon/test/Pibronic/test/test_models/'
 
-#     data.states = 2
-#     data.modes = 2
+    def test_load(self):
+        # load the relevant data
+        data = pm.BoxData()
+        data.id_data = self.id_data
+        data.id_rho = self.id_rho
 
-#     data.samples = samples
-#     data.beads = 1000
-#     data.temperature = 300.0
-#     data.blocks = samples // Bsize
-#     data.block_size = Bsize
+        # should be able to pass a data object to file_structure
+        files = fs.FileStructure.from_boxdata(self.test_path, data)
+        data.path_vib_model = files.path_vib_model
+        data.path_rho_model = files.path_rho_model
 
-#     # setup empty tensors, models, and constants
-#     data.preprocess()
+        data.states = 2
+        data.modes = 2
 
-#     # store results here
-#     results = BoxResult(data=data)
-#     results.path_root = files.path_rho_results
+        data.samples = self.samples
+        data.beads = 10
+        data.temperature = 300.0
+        data.blocks = self.samples // self.block_size
+        data.block_size = self.block_size
 
-#     block_compute(data, results)
-#     return
+        # setup empty tensors, models, and constants
+        data.preprocess()
+
+        # store results here
+        results = pm.BoxResult(data=data)
+        results.path_root = files.path_rho_results
+
+        pm.block_compute(data, results)
+
+        return
 
 
 if __name__ == '__main__':
